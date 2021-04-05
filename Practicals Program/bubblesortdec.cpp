74947753ae23d38329fca5d8550d7af2718cdf6c
Aditya <37655818+adi-075@users.noreply.github.com>
2021-04-05 17:27:04 +0530

    Add C++ Practicals Programs
    Added C++ practical programs

---
+++ Practicals Program/bubblesortdec.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a[10], n, i, j, tmp; //Initialisation
+    cout << "Enter number of elements of an array:\t";
+    cin >> n;
+    cout << "Enter Elements of array:\t";
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    //bubble sort
+    //First Iteration
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1-i;j++)
+        {
+            if(a[j] < a[j+1])
+            {
+                tmp = a[j];
+                a[j]=a[j+1];
+                a[j=1]=tmp;
+            }
+        }
+    }
+    //Output
+    cout << "Sorted Array Elements:\t";
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<"\t";
+    }
+    return 0;
+}
